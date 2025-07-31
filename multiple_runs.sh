06a778c43f486b8e91c6d4196dfb477d02957440
Auguste De Lambilly <auguste.delam@yahoo.fr>
2025-07-31 07:19:03 +0200

    update the description of multiple_run

--- multiple_runs.sh
+++ multiple_runs.sh
@@ -3,7 +3,7 @@
 
 if [[ "$1" == "--help" ]]; then
     echo "Usage:"
-    echo "  ./multiple_runs.sh NB LOG MUL BASE SYS ENV G R B ALG [GPU] [MOD]"
+    echo "  ./multiple_runs.sh NB LOG MUL BASE SYS ENV G K Norm R B ALG MOD GPU"
     echo ""
     echo "Arguments:"
     echo "  NB   : Number of samples per run (default: 20)"
@@ -22,7 +22,7 @@ if [[ "$1" == "--help" ]]; then
     echo "  MOD  : Mode for the environment loss (default: None which means l1)"
     echo ""
     echo "Example:"
-    echo "  ./multiple_runs.sh 20 log.txt 50 /Data/auguste.de-lambilly/mattergenbis/ Li-Co-O "'Co-O':3" 1.0 0.01 True 3 2 True 0 huber"
+    echo " bash multiple_runs.sh 20 log.txt 50 /Data/auguste.de-lambilly/mattergenbis/ Li-Co-O "'Co-O':6" 0.0001 0.0001 True 3 2 False huber 0"
     exit 0
 fi
 
